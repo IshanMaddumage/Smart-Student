a7c5dd626ccad4ee009873247b09c15139c389df
Ishan Maddumage <ishan.tulaksha00@gmail.com>
2022-12-02 18:24:47 +0530

    2022/12/02

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,14 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_student_academy/controllers/auth_controller.dart';
 
 class AuthProvider extends ChangeNotifier {
   //auth contraller instance
-  final AuthController _authController = AuthController();
+  // final AuthController _authController = AuthController();
 
-  //store firebase user
-  User? _firebaseUser;
+  // //store firebase user
+  // User? _firebaseUser;
 
   //getter for firebase user
-  
+
 }
